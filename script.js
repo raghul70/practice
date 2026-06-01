2350b6e698fb09b1d9c419f01f827f1c34a3922f
raghul70 <raghulakash007@gmail.com>
2026-06-01 14:26:20 +0530

    modified

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-let picker = document.getElementById("colorPicker");
-
-picker.addEventListener("input", function () {
-    document.body.style.backgroundColor = picker.value;
-});
+console.log("Hello JavaScript");
